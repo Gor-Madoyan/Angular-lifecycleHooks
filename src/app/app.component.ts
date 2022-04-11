0320f3517a4e464ad267ed0c3e1788e1143a8040
EPAM\Gor_Madoyan <Gor Madoyan>
2022-04-11 11:43:54 +0400

    angular lesson2 lifecylce hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, OnDestroy} from '@angular/core';
+import {ParentComponent} from "./parent/parent.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy{
   title = 'lifecycleHooks';
+
+
+  @ViewChild(ParentComponent) child: ParentComponent | undefined
+  ngAfterContentInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.child);
+
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
 }
